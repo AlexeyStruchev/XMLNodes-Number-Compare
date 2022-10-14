b565b3a5c4edfab5e3d101182f6b7aa7b29c1b3f
AStruchev <astruchev@gmail.com>
2022-10-14 11:03:04 +0300

    - simplified xml examples

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-"""
-The utility checks if number of nodes in files of different languages is equal or not.
+""" The utility checks if number of nodes in files of different languages is equal or not.
 It is assumed that nodes number in files of different languages should be equal.
 If nodes number is different it is a sign of a defect.
 In order to correctly compare files there has to be at least two files by different languages each.
 By default, the utility works with files attached to this project. To change default behavior create a folder
 and place xml files there.
 A file template is a file name with language inside the name. Language can be in any place of the file and consists of
-two symbols: en - English, es - Spanish, etc.
-There is an example of absolute path to a folder: r'C:\Users\user\Documents\Ixml\26167\CCS_Feed_26167_210111_0815'
-"""
+two symbols: en - English, es - Spanish, etc. """
 
 from compare_xml_functions import compare_xml_files_by_nodes
 
@@ -21,10 +18,12 @@ nodes_to_compare = \
     [
         '/Feed',
         '/Feed/Product',
-        '/Feed/Product/CustomerSKUID',
-        '/Feed/Product/Identifiers',
-        '/Feed/Product/Identifiers/UNSPSC',
-        '/Feed/Product/Identifiers/UNSPSC/CommodityName'
+        '/Feed/Product/INTSKUID',
+        '/Feed/Product/ManufacturerPN',
+        '/Feed/Product/Manufacturer',
+        '/Feed/Product/ProductName',
+        '/Feed/Product/ProductName',
+        '/Feed/Product/ProductTitle'
     ]
 
 compare_xml_files_by_nodes(file_template, langs, folder, nodes_to_compare, file_ext)
